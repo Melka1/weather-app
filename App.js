e0a313b0f5862416a5aac6d9830cc48eacd2b5f4
melka1 <melkatole1@gmail.com>
2023-04-22 02:34:41 +0300

    Weather App in progress

---
+++ App.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import CurrentWeather from './src/components/CurrentWeather'
+import UpcomingWeather from './src/components/UpcomingWeather'
+
+
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* <CurrentWeather/> */}
+      <UpcomingWeather/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  }
+})
